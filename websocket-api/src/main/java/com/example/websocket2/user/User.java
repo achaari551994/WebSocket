d7668ec338c15eddec088a1bae5fe41c79d7957b
AHMED CHAARI <ahmed.chaari@talan.com>
2024-01-31 15:18:40 +0100

    init project

---
+++ websocket-api/src/main/java/com/example/websocket2/user/User.java
@@ -0,0 +1,13 @@
+package com.example.websocket2.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class User {
+    private String nickName;
+    private String fullName;
+    private Status status;
+}
